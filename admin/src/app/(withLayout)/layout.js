6b8645bc375ae1130da024f165399cf4e3c9546e
kumarmudur <kumarmudur10@gmail.com>
2026-06-30 21:17:36 -0400

    global css changes

---
+++ admin/src/app/(withLayout)/layout.js
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
+const WithLayout = ({ children }) => {
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-2">
+        <Sidebar />
+      </div>
+      <div className="col-span-10 p-6 my-6 mr-8 border-[1] border-gray-300 rounded-xl shadow-lg">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default WithLayout;
